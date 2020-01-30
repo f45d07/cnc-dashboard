a122d6d10860954c8ca31e0d5b577a3afabbce50
f45d07 <f45d07@users.noreply.github.com>
2020-01-30 04:49:22 +0300

    All

---
+++ build/precache-manifest.99986036e648840dbbe7a8da17f3f8c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e98d772c6a1366364b2d57a0c3f79c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f07287e7377c8672b639",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "afe8e9fdbab674d77db0",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f07287e7377c8672b639",
+    "url": "/static/js/2.ef6f67cd.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.ef6f67cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "afe8e9fdbab674d77db0",
+    "url": "/static/js/main.66c48bff.chunk.js"
+  },
+  {
+    "revision": "d308f9a6c00b80418fa7",
+    "url": "/static/js/runtime-main.3471e2a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
